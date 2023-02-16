ec4a980c5c381cf9258375a4d2bb1c75d3f51b0d
kimki0917 <kimki0917@naver.com>
2023-02-16 16:24:02 +0900

    css Error fin

--- jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
+++ jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -1,27 +1,23 @@
 package com.douzone.jblog.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.douzone.jblog.config.web.FileuploadConfig;
 import com.douzone.jblog.config.web.MessageSourceConfig;
 import com.douzone.jblog.config.web.MvcConfig;
 import com.douzone.jblog.config.web.SecurityConfig;
+
 @Configuration
 @EnableAspectJAutoProxy
-@ComponentScan({"com.douzone.jblog.controller"})
-@Import({MvcConfig.class, SecurityConfig.class, MessageSourceConfig.class, FileuploadConfig.class})
+@ComponentScan({ "com.douzone.jblog.controller" })
+@Import({ MvcConfig.class, SecurityConfig.class, MessageSourceConfig.class, FileuploadConfig.class })
 public class WebConfig implements WebMvcConfigurer {
 	// Site Inteceptor
 
-
-		
-		// Application Context Event Listener
+	// Application Context Event Listener
 
 }
